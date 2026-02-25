31e8d30774fc4f7d255f0108e3a9e9026de81dd4
unknown <ricchini.santiagotomas@gmail.com>
2026-02-25 15:31:23 -0300

    conexion a la db exitosa, la pool esta configurada con .env

---
+++ server.js
@@ -0,0 +1,17 @@
+import mysql from "mysql2/promise"
+import dotenv from 'dotenv'
+
+
+dotenv.config()
+
+const pool = mysql.createPool({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE
+})
+
+const [result] = await pool.query("SELECT * FROM usuarios WHERE last_name = 'ricchini';");
+console.log(result);
+
+
